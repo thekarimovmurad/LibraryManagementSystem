673cc4f5ae43a3e22183c8668eecc04571d85745
thekarimovmurad <thekarimovmurad@gmail.com>
2024-01-24 20:06:19 +0100

    repeat update

--- LibraryManagementSystem_scripts.sql
+++ LibraryManagementSystem_scripts.sql
@@ -42,11 +42,75 @@ create table TransactionsHistory
 
 );
 
-select *
-from Member
-select *
-from Book
-select *
-from Transactions
-select *
-from TransactionsHistory
+--#region Repeat
+/*Select*/
+select * from Member
+select * from Book
+select * from Transactions
+select * from TransactionsHistory
+select distinct numberOfBooks from Book
+
+/*Where (and, or, not)*/
+select * from Member
+where totalFine !=0
+select * from Member
+where totalFine !=0 and numBooks<3
+select * from Member
+where totalFine !=0 or numBooks<3
+select * from Member
+where totalFine !=0 or numBooks<3
+select bookName from Book
+where Book.author is not null
+
+/*Order by*/
+select * from Book
+order by price
+select * from Book
+order by bookName desc
+select * from Book
+order by bookName desc, bookId asc
+
+/*Update, Delete, Create*/
+update Book set price=12.99, bookName='Programming in C#'
+where Book.bookId=3
+delete from Book
+where Book.bookId=3
+insert into Book(bookId, bookName, author, price, numberOfBooks)
+values (3, 'Programming in C#', 'David Williams', 49.99, 8)
+select *from Book
+where Book.bookId=3
+
+/*Min, Max, Sum, Avg*/
+select min(Book.price) from Book
+select max(Book.price) from Book
+select sum(Book.price) from Book
+select avg(Book.price) from Book
+
+/*Like, Wildcard*/
+select  * from Member
+where memberName like '%a%'
+select  * from Member
+where memberName like '[abs]%'
+select  * from Member
+where memberName like '[a-g]%'
+
+/*In*/
+select  * from Member
+where memberType in('student')
+select  * from Member
+where memberType not in('student')
+select  * from Member
+where memberType in(select memberType from Member where memberId=1)
+/*Between*/
+select  * from Book
+where price between 10 and 20
+select  * from Book
+where price between 10 and 20
+select  * from Transactions
+where issueDate not between '2024/01/5' and '2024/01/8'
+
+
+/*As*/
+select bookId as Id from Book
+/**/
+--#endregion
